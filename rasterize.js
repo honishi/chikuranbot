0bb8000bc23800fefb14157f1fa642a71f5eb4a7
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-05-09 03:32:34 +0900

    improved ad removal feature.

--- rasterize.js
+++ rasterize.js
@@ -9,25 +9,52 @@ var page = require('webpage').create(),
 if (system.args.length !== 4 ) {
     console.log('Usage: rasterize.js useragent URL filename');
     phantom.exit(1);
-} else {
+}
+else {
     ua = system.args[1];
     address = system.args[2];
     output = system.args[3];
     page.viewportSize = { width: width, height: height };
     page.clipRect = { left: clipLeft, top: clipTop, width: clipWidth, height: clipHeight };
     page.settings.userAgent = ua
+
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
             phantom.exit(1);
-        } else {
-            page.evaluate(function() {
-                var ads = document.getElementsByClassName('geenee');
-                for (var i=0; i<ads.length; i++) {
-                    ads[i].parentNode.removeChild(ads[i]);
-                }
-            });
+        }
+        else {
             window.setTimeout(function () {
+                console.log('rasterizing...');
+
+                /* for console log in page.evaluate(). */
+                page.onConsoleMessage = function (message){
+                    console.log(message);
+                };
+
+                page.evaluate(function(){
+                    function removeElementsByClassName(className) {
+                        console.log('removing elements by class name: ' + className);
+                        var elements = document.getElementsByClassName(className);
+                        for (var i=0; i<elements.length; i++) {
+                            elements[i].parentNode.removeChild(elements[i]);
+                            console.log('removed.');
+                        }
+                    }
+
+                    function removeElementById(idName) {
+                        console.log('removing element by id name: ' + idName);
+                        var element = document.getElementById(idName);
+                        if (element) {
+                            element.parentNode.removeChild(element);
+                            console.log('removed.');
+                        }
+                    }
+
+                    removeElementsByClassName('geenee');
+                    removeElementById('ninja_iframe_1');
+		});
+
                 page.render(output);
                 phantom.exit();
             }, wait);
