7a6cf7a050be1e0253dde43edd1e8a39df0f7622
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-10-18 16:37:29 +0900

    updated pyenv settings.

--- go.sh
+++ go.sh
@@ -11,8 +11,11 @@ url_chikuran='http://www.chikuwachan.com/live/'
 output=/tmp/temp.png
 
 cd ${basedir}
+# pwd
 
 source ./go.env
+# pyenv versions
+# which python
 
 phantomjs ./rasterize.js "${user_agent}" "${url_chikuran}" ${output}
 ./tweet.py "${consumer_key}" "${consumer_secret}" "${access_key}" "${access_secret}" "${output}"
